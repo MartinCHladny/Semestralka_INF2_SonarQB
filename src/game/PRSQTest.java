544489a421861c8dc35bf9db08de5951c14764f6
martin <chladny6@stud.uniza.sk>
2026-02-04 09:52:27 +0100

    PRSQ

---
+++ src/game/PRSQTest.java
@@ -0,0 +1,16 @@
+package game;
+
+public class PRSQTest {
+    public PRSQTest() {
+    }
+
+    public void doSmothing() {
+        int cislo = 0;
+        while (true) {
+            if (cislo == 1) {
+                return;
+            }
+            cislo++;
+        }
+    }
+}
