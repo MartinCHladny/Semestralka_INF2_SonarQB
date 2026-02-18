f0d8e18bfe4a21a7223f2297ca99d3040f64fff7
MartinCHladny <150118171+MartinCHladny@users.noreply.github.com>
2026-02-18 10:05:47 +0100

    Update SampleTest.java

--- test/SampleTest.java
+++ test/SampleTest.java
@@ -1,10 +1,26 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 public class SampleTest {
 
     @Test
     void simpleTest() {
         assertEquals(4, 2 + 2);
+    }
+}
+
+public class PRSQTestLoopTest {
+
+    @Test
+    void testDoSmothingTimeout() {
+        PRSQTest sq = new PRSQTest();
+
+        // Stop the method if it runs longer than 2 seconds
+        assertTimeoutPreemptively(Duration.ofSeconds(2), () -> {
+            sq.doSmothing();
+        }, "doSmothing() ran longer than expected (infinite loop detected)");
     }
 }
